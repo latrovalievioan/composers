49202457966c10d435a64d635d0c09b8592f0cd1
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-06-07 18:44:19 +0300

    feat: implement searchbar functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/App.css';
-import { essentials, popular } from './redux/actions';
+import { essentials, popular, nameSearch } from './redux/actions';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { Composer } from './types';
 import { ComposerCard } from './components/ComposerCard';
@@ -8,9 +8,20 @@ import { ComposerCard } from './components/ComposerCard';
 function App() {
   const dispatch = useAppDispatch();
   const composerList = useAppSelector((state) => state.composerList);
+  const searchBar = React.useRef<HTMLInputElement>(null);
+
   return (
     <div className="App">
       <h1 className="page-title">Classical Composers</h1>
+      <input
+        ref={searchBar}
+        onInput={() => {
+          if (searchBar.current) dispatch(nameSearch(searchBar.current.value));
+        }}
+        type="text"
+        placeholder="Find a Composer"
+        className="search-bar"
+      />
       <nav>
         <button onClick={() => dispatch(essentials())}>
           Essential Composers
