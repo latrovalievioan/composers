05598cfd33516a0a04de64cc7ae566757a39496f
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-06-08 23:00:30 +0300

    fix: add types to api calls

--- src/api.tsx
+++ src/api.tsx
@@ -1,13 +1,15 @@
+import { Composer } from './types';
+
 const rootUrl = 'https://api.openopus.org';
 
-export const getEssentialComposers = async () => {
+export const getEssentialComposers = async (): Promise<readonly Composer[]> => {
   const response = await fetch(`${rootUrl}/composer/list/rec.json`);
   const data = await response.json();
   const composers = data.composers;
   return composers;
 };
 
-export const getPopularComposers = async () => {
+export const getPopularComposers = async (): Promise<readonly Composer[]> => {
   const response = await fetch(`${rootUrl}/composer/list/pop.json`);
   const data = await response.json();
   const composers = data.composers;
@@ -15,12 +17,11 @@ export const getPopularComposers = async () => {
   return composers;
 };
 
-export const searchComposer = async (name: string) => {
+export const searchComposer = async (
+  name: string
+): Promise<readonly Composer[]> => {
   const response = await fetch(`${rootUrl}/composer/list/search/${name}.json`);
   const data = await response.json();
-  if (data.status.success === 'false') {
-    return data.status.error;
-  }
   const composers = data.composers;
   return composers;
 };
