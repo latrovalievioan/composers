e3ccd58d621dfe9661328f2ef2883f68fcbd62af
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-06-08 21:27:26 +0300

    feat: add router to project

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,44 @@ import './App.css';
 import { essentials, popular, nameSearch, favorites } from './redux/actions';
 import { useAppDispatch } from './hooks';
 import { ComposersGrid } from './components/ComposersGrid';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   const dispatch = useAppDispatch();
   const searchBar = React.useRef<HTMLInputElement>(null);
 
   return (
-    <div className="App">
-      <h1 className="page-title">Classical Composers</h1>
-      <input
-        ref={searchBar}
-        onInput={() => {
-          if (searchBar.current) dispatch(nameSearch(searchBar.current.value));
-        }}
-        type="text"
-        placeholder="Find a Composer"
-        className="search-bar"
-      />
-      <nav className="buttons">
-        <button onClick={() => dispatch(essentials())} className="nav-button">
-          Essential Composers
-        </button>
-        <button onClick={() => dispatch(popular())} className="nav-button">
-          Popular Composers
-        </button>
-        <button onClick={() => dispatch(favorites())} className="nav-button">
-          Favorite Composers
-        </button>
-      </nav>
-      <ComposersGrid />
-    </div>
+    <Router>
+      <div className="App">
+        <h1 className="page-title">Classical Composers</h1>
+        <input
+          ref={searchBar}
+          onInput={() => {
+            if (searchBar.current)
+              dispatch(nameSearch(searchBar.current.value));
+          }}
+          type="text"
+          placeholder="Find a Composer"
+          className="search-bar"
+        />
+        <nav className="buttons">
+          <Link to="/essentials">Essential Composers</Link>
+          <Link to="/popular">Popular Composers</Link>
+          <Link to="/favorites">Favorite Composers</Link>
+        </nav>
+        <Switch>
+          <Route path="/essentials">
+            <ComposersGrid />
+          </Route>
+          <Route path="/popular">
+            <ComposersGrid />
+          </Route>
+          <Route path="/favorites">
+            <ComposersGrid />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/redux/reducers/favoriteComposersReducer.ts
@@ -0,0 +1,19 @@
+export const favoriteComposersReducer = (
+  state = new Set<string>(),
+  action: any
+) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITES': {
+      const newState = new Set(state);
+      newState.add(action.payload);
+      return newState;
+    }
+    case 'REMOVE_FROM_FAVORITES': {
+      const newState = new Set(state);
+      newState.delete(action.payload);
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
