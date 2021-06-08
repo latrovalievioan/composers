f1e97872c4c7f2ff79e93fbceaec478410c67b8a
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-06-08 11:16:42 +0300

    refactor: card overlay is now a separate component

---
+++ src/components/CardOverlay.tsx
@@ -0,0 +1,59 @@
+import { ReactElement } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { Composer } from '../types';
+
+export const CardOverlay = ({
+  composer,
+}: {
+  composer: Composer;
+}): ReactElement => {
+  const isFavorite = (): boolean => {
+    const storageComposers = JSON.parse(
+      localStorage.getItem('composers') || '[]'
+    );
+    return storageComposers.some((comp: Composer) => comp.id === composer.id);
+  };
+
+  const addToFavorites = () => {
+    const storageComposers = JSON.parse(
+      localStorage.getItem('composers') || '[]'
+    );
+    if (isFavorite()) return;
+    storageComposers.push(composer);
+    localStorage.setItem('composers', JSON.stringify(storageComposers));
+  };
+
+  const removeFromFavorites = () => {
+    const storageComposers = JSON.parse(
+      localStorage.getItem('composers') || '[]'
+    );
+    if (!isFavorite()) return;
+    const filteredComposers = storageComposers.filter(
+      (comp: Composer) => comp.id !== composer.id
+    );
+    localStorage.setItem('composers', JSON.stringify(filteredComposers));
+  };
+
+  const getSign = () => {
+    return isFavorite() ? (
+      <>
+        {' '}
+        <span className="plus-icon" onClick={() => removeFromFavorites()}>
+          <FontAwesomeIcon icon={faMinusCircle} size="3x" />
+        </span>
+        <p>Remove from Favorites</p>
+      </>
+    ) : (
+      <>
+        {' '}
+        <span className="plus-icon" onClick={() => addToFavorites()}>
+          <FontAwesomeIcon icon={faPlusCircle} size="3x" />
+        </span>
+        <p>Add to Favorites</p>
+      </>
+    );
+  };
+
+  return <div className="image-overlay">{getSign()}</div>;
+};
--- src/components/ComposerCard.tsx
+++ src/components/ComposerCard.tsx
@@ -1,59 +1,17 @@
 import React, { ReactElement } from 'react';
 import { Composer } from '../types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 import '../styles/ComposerCard.css';
-import { useAppDispatch } from '../hooks';
+import { CardOverlay } from './CardOverlay';
 
 export const ComposerCard = ({
   composer,
 }: {
   composer: Composer;
 }): ReactElement => {
-  const storageComposers = JSON.parse(
-    localStorage.getItem('composers') || '[]'
-  );
-
-  const isFavorite = () => {
-    return storageComposers.some((comp: Composer) => comp.id === composer.id);
-  };
-
-  const addToFavorites = () => {
-    if (isFavorite()) return;
-    storageComposers.push(composer);
-    localStorage.setItem('composers', JSON.stringify(storageComposers));
-  };
-
-  const removeFromFavorites = () => {
-    if (!isFavorite()) return;
-    const filteredComposers = storageComposers.filter(
-      (comp: Composer) => comp.id !== composer.id
-    );
-    localStorage.setItem('composers', JSON.stringify(filteredComposers));
-  };
-
   return (
     <div className="composer-card">
       <img src={composer.portrait} alt=""></img>
-      <div className="image-overlay">
-        {isFavorite() ? (
-          <>
-            {' '}
-            <span className="plus-icon" onClick={() => removeFromFavorites()}>
-              <FontAwesomeIcon icon={faMinusCircle} size="3x" />
-            </span>
-            <p>Remove from Favorites</p>
-          </>
-        ) : (
-          <>
-            {' '}
-            <span className="plus-icon" onClick={() => addToFavorites()}>
-              <FontAwesomeIcon icon={faPlusCircle} size="3x" />
-            </span>
-            <p>Add to Favorites</p>
-          </>
-        )}
-      </div>
+      <CardOverlay composer={composer} />
       <div className="composer-info">
         <h3 className="composer-title">{composer.complete_name}</h3>
         <span>Birth: {composer.birth}</span>
