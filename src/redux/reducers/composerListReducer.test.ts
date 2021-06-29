c36e19a0af3afae18d9703efd938accceff39607
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-06-29 21:00:00 +0300

    write tests for esential composers and popular composers with the composerListReducer

--- src/redux/reducers/composerListReducer.test.ts
+++ src/redux/reducers/composerListReducer.test.ts
@@ -11,4 +11,68 @@ describe('Composer list Reducer testings', () => {
       popularComposers: [],
     });
   });
+
+  it('Essential Composers action should set the state to an object with two keys: essentialComposers: an array with composer objects, popularComposer: an empty array', () => {
+    const state = composerListReducer(initialState, {
+      type: 'SET_ESSENTIAL_COMPOSERS',
+      payload: [
+        {
+          id: '23',
+          name: 'ioni',
+          complete_name: 'ioni bonboni',
+          birth: '30.04.1996',
+          death: '',
+          epoch: 'post-modern',
+          portrait: 'https://i.ytimg.com/vi/cNycdfFEgBc/maxresdefault.jpg',
+        },
+      ],
+    });
+
+    expect(state).toStrictEqual({
+      essentialComposers: [
+        {
+          id: '23',
+          name: 'ioni',
+          complete_name: 'ioni bonboni',
+          birth: '30.04.1996',
+          death: '',
+          epoch: 'post-modern',
+          portrait: 'https://i.ytimg.com/vi/cNycdfFEgBc/maxresdefault.jpg',
+        },
+      ],
+      popularComposers: [],
+    });
+  });
+
+  it('Popular Composers action should set the state to an object with two keys: popularComposers: an array with composer objects, essentialComposers: an empty array', () => {
+    const state = composerListReducer(initialState, {
+      type: 'SET_POPULAR_COMPOSERS',
+      payload: [
+        {
+          id: '23',
+          name: 'ioni',
+          complete_name: 'ioni bonboni',
+          birth: '30.04.1996',
+          death: '',
+          epoch: 'post-modern',
+          portrait: 'https://i.ytimg.com/vi/cNycdfFEgBc/maxresdefault.jpg',
+        },
+      ],
+    });
+
+    expect(state).toStrictEqual({
+      popularComposers: [
+        {
+          id: '23',
+          name: 'ioni',
+          complete_name: 'ioni bonboni',
+          birth: '30.04.1996',
+          death: '',
+          epoch: 'post-modern',
+          portrait: 'https://i.ytimg.com/vi/cNycdfFEgBc/maxresdefault.jpg',
+        },
+      ],
+      essentialComposers: [],
+    });
+  });
 });
