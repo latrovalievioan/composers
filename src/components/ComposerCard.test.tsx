78677f498ce5dd6e80a008737c5f79cd76e5bf86
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-07-01 15:20:55 +0300

    test: write unit tests for ComposerCard component

--- src/components/ComposerCard.test.tsx
+++ src/components/ComposerCard.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { ComposerCard } from './ComposerCard';
+import { AddRemoveButton } from './AddRemoveButton';
+import { shallow, configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+configure({ adapter: new Adapter() });
 
 jest.mock('./AddRemoveButton', () => {
   return { AddRemoveButton: () => <div id="mock-button"></div> };
@@ -23,4 +27,40 @@ it('renders correctly', () => {
     )
     .toJSON();
   expect(nav).toMatchSnapshot();
+});
+
+describe('Testing Composer Card component with shallow', () => {
+  const composer = {
+    id: '87',
+    name: 'Bach',
+    complete_name: 'Johann Sebastian Bach',
+    birth: '1685-01-01',
+    death: '1750-01-01',
+    epoch: 'Baroque',
+    portrait: 'https://assets.openopus.org/portraits/12091447-1568084857.jpg',
+  };
+
+  const card = shallow(<ComposerCard composer={composer} />);
+
+  it('Card has an Image of the composer', () => {
+    expect(card.find('img')).toHaveLength(1);
+  });
+
+  it('Card has a composer info wrapper', () => {
+    expect(card.exists('.composer-info')).toEqual(true);
+  });
+
+  const composerInfoWrapper = card.find('.composer-info');
+
+  it('Composer info wrapper contains a title', () => {
+    expect(composerInfoWrapper.find('h3')).toHaveLength(1);
+  });
+
+  it('Composer info wrapper contain three spans', () => {
+    expect(composerInfoWrapper.find('span')).toHaveLength(3);
+  });
+
+  it('Composer info wrapper contains one AddRemoveButton Component', () => {
+    expect(composerInfoWrapper.find(AddRemoveButton)).toHaveLength(1);
+  });
 });
